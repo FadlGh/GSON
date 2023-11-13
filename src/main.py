745bf1a0335027103ab6f27bea2dc4e6835a7a85
FadlGh <fadl2009gh@gmail.com>
2023-11-13 21:05:43 +0200

    visualize bar function

--- src/main.py
+++ src/main.py
@@ -27,8 +27,40 @@ def visualize_plot(src: str, colors: list, linestyle: str = 'solid', linewidth: 
     plt.title(title)
     plt.show()
 
-def visualize_bar(src):
+def visualize_bar(src: str, colors: list, tick_labels: str = None, title: str = 'Bars', subset_key: str = None):
     with open(src) as f:
         data = json.load(f)
+    
+    if subset_key:
+        if subset_key in data:
+            data = {subset_key: data[subset_key]}
+        else:
+            raise KeyError(f"The specified subset key '{subset_key}' does not exist in the JSON data.")
+    
+    left = []
+    for i in range(len(data)):
+        left.append(i)
 
-visualize_plot('src/main.json', colors = ['green'])
+
+    values = list(data.values())
+    height_values = []
+
+    for j in range(len(values)):
+        height_values.append(values[j])
+
+    print(height_values)
+    
+    if tick_labels:
+        tick_labels = tick_labels[:len(values)]
+        plt.bar(left, height_values, color = colors, tick_labels = tick_labels)
+    else:
+        plt.bar(left, values, color = colors)
+
+    plt.xlabel('x - axis')
+    plt.ylabel('y - axis')
+
+    plt.title(title)
+    plt.show()
+
+
+visualize_bar('src/bar.json', colors = ['green'])
