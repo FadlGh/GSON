b63eafa9d5f21d9aec40fb1ca5928c7fb524c9fc
FadlGh <fadl2009gh@gmail.com>
2023-11-13 20:24:59 +0200

    Added sub directory option

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
-import matplotlib.pyplot as plt 
 import json
+import matplotlib.pyplot as plt
 
-def visualize_plot(src: str, colors: list, linestyle: str = 'solid', linewidth: int = 1, title: str = 'Graph'):
+def visualize_plot(src: str, colors: list, linestyle: str = 'solid', linewidth: int = 1, title: str = 'Graph', subset_key: str = None):
     with open(src) as f:
         data = json.load(f)
+
+    if subset_key:
+        if subset_key in data:
+            data = {subset_key: data[subset_key]}
+        else:
+            raise KeyError(f"The specified subset key '{subset_key}' does not exist in the JSON data.")
 
     for i, inner_dict in enumerate(data.values()):
         values_list = list(inner_dict.values())
@@ -11,14 +17,18 @@ def visualize_plot(src: str, colors: list, linestyle: str = 'solid', linewidth: 
         y_values = values_list[1]
 
         if i >= len(colors):
-            plt.plot(x_values, y_values, linestyle = linestyle, linewidth = linewidth)
+            plt.plot(x_values, y_values, linestyle=linestyle, linewidth=linewidth)
         else:
-            plt.plot(x_values, y_values, color = colors[i], linestyle = linestyle, linewidth = linewidth)
+            plt.plot(x_values, y_values, color=colors[i], linestyle=linestyle, linewidth=linewidth)
 
-    plt.xlabel('x - axis') 
-    plt.ylabel('y - axis') 
-    
+    plt.xlabel('x - axis')
+    plt.ylabel('y - axis')
+
     plt.title(title)
     plt.show()
+
+def visualize_bar(src):
+    with open(src) as f:
+        data = json.load(f)
 
 visualize_plot('src/main.json', colors = ['green'])
