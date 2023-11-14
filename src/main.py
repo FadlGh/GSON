4c6a723ab095a35db2a6a7343369ce37230e5cb4
FadlGh <fadl2009gh@gmail.com>
2023-11-14 19:40:38 +0200

    fixed visualize bar and began with pie

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import json
 import matplotlib.pyplot as plt
 
-def visualize_plot(src: str, colors: list, linestyle: str = 'solid', linewidth: int = 1, title: str = 'Graph', subset_key: str = None):
+def visualize_plot(src: str, colors: list, linestyle: str = 'solid', linewidth: int = 1, title: str = 'Graph', x_axis: str = 'x-axis', y_axis: str = 'y-axis', subset_key: str = None):
     with open(src) as f:
         data = json.load(f)
 
@@ -21,46 +21,35 @@ def visualize_plot(src: str, colors: list, linestyle: str = 'solid', linewidth: 
         else:
             plt.plot(x_values, y_values, color=colors[i], linestyle=linestyle, linewidth=linewidth)
 
-    plt.xlabel('x - axis')
-    plt.ylabel('y - axis')
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
 
     plt.title(title)
     plt.show()
 
-def visualize_bar(src: str, colors: list, tick_labels: str = None, title: str = 'Bars', subset_key: str = None):
+def visualize_bar(src: str, colors: list, title: str = 'Bars', x_axis: str = 'x-axis', y_axis: str = 'y-axis', subset_key: str = None):
     with open(src) as f:
         data = json.load(f)
     
     if subset_key:
         if subset_key in data:
-            data = {subset_key: data[subset_key]}
+            data = data[subset_key]
         else:
             raise KeyError(f"The specified subset key '{subset_key}' does not exist in the JSON data.")
     
-    left = []
-    for i in range(len(data)):
-        left.append(i)
-
-
+    keys = list(data.keys())
     values = list(data.values())
-    height_values = []
 
-    for j in range(len(values)):
-        height_values.append(values[j])
+    plt.bar(keys, values, color=colors)
 
-    print(height_values)
-    
-    if tick_labels:
-        tick_labels = tick_labels[:len(values)]
-        plt.bar(left, height_values, color = colors, tick_labels = tick_labels)
-    else:
-        plt.bar(left, values, color = colors)
-
-    plt.xlabel('x - axis')
-    plt.ylabel('y - axis')
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
 
     plt.title(title)
     plt.show()
+
+def visualize_pie():
+    pass
 
 
 visualize_bar('src/bar.json', colors = ['green'])
