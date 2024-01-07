6ab7b90eb4c5528e2b9e8b650c65a6f91f489513
FadlGh <fadl2009gh@gmail.com>
2024-01-07 19:06:24 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='GSON',
+   version='1.0',
+   description='Visualize JSON data as plots, pies, bars',
+   author='Fadl Ghaddar',
+   author_email='fadl2009gh@gmail.com',
+   packages=['GSON'],
+   install_requires=['matplotlib']
+)
